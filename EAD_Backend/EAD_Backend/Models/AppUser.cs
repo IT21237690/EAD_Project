46cb8db8d3c7107cfd617b76eaa5123f2a05a90b
Minidu Tissera <IT21237690@my.sliit.lk>
2024-09-28 05:06:00 +0530

    Initial commit

---
+++ EAD_Backend/EAD_Backend/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EAD_Backend.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(150)")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
